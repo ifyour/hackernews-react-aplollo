ad389b3f59b69fb1f66780b007646570c1456c56
王明明 <wb-wmm393051@antfin.com>
2019-11-28 12:11:46 +0800

    post link

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink'
 
 function App() {
-  return <LinkList />
+  // return <LinkList />
+  return <CreateLink />
 }
 
 export default App;
