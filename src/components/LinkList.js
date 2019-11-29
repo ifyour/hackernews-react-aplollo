0e5186f29411ea43ce703b749511ba187c534abe
王明明 <wb-wmm393051@antfin.com>
2019-11-29 12:43:06 +0800

    add vote

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -12,6 +12,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -27,8 +37,8 @@ class LinkList extends Component {
           const linksToRender = data.feed.links;
           return (
             <div>
-              {linksToRender.map(link => (
-                <Link key={link.id} link={link} />
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
               ))}
             </div>
           );
