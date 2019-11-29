68d9b2d1a89aa729bf8a6483690c0478ca855e84
王明明 <wb-wmm393051@antfin.com>
2019-11-29 16:13:35 +0800

    add new Link & vote Subscriptions

--- src/index.js
+++ src/index.js
@@ -9,25 +9,50 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
-import { setContext } from 'apollo-link-context'
-import { AUTH_TOKEN } from './constants'
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
+  const token = localStorage.getItem(AUTH_TOKEN);
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+      authorization: token ? `Bearer ${token}` : ""
     }
-  }
-})
+  };
+});
+
+// split 用来路由一个混合请求到指定的中间件
+// 这里的作用就是转发普通 httpLink 还是 wsLink
+// 第一个为 test 函数，为 true 则使用参数二，否则参数三
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink) // 先授权，再请求，注意中间件顺序
+);
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink), // 先授权，再请求，注意中间件顺序
+  link,
   cache: new InMemoryCache()
 });
 
