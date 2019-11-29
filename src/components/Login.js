a061d0e83753c2c2716c2dbcbb70c154917f444d
ifyour <ifyour@outlook.com>
2019-11-30 00:24:43 +0800

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,6 +27,16 @@ class Login extends Component {
     name: ""
   };
 
+  _confirm = async data => {
+    const { token } = this.state.login ? data.login : data.signup;
+    this._saveUserData(token);
+    this.props.history.push(`/`);
+  };
+
+  _saveUserData = token => {
+    localStorage.setItem(AUTH_TOKEN, token);
+  };
+
   render() {
     const { login, email, password, name } = this.state;
     return (
@@ -78,15 +88,6 @@ class Login extends Component {
     );
   }
 
-  _confirm = async data => {
-    const { token } = this.state.login ? data.login : data.signup;
-    this._saveUserData(token);
-    this.props.history.push(`/`);
-  };
-
-  _saveUserData = token => {
-    localStorage.setItem(AUTH_TOKEN, token);
-  };
 }
 
 export default Login;
