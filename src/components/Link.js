81126f5476e0d2657102a1dffb2da53f29344c62
王明明 <wb-wmm393051@antfin.com>
2019-11-29 16:13:11 +0800

    fix: UI fails to re-render after cache.writeQuery

--- src/components/Link.js
+++ src/components/Link.js
@@ -8,8 +8,8 @@ import { timeDifferenceForDate } from "../utlis";
 const VOTE_MUTATION = gql`
   mutation VoteMutation($linkId: ID!) {
     vote(linkId: $linkId) {
-      id
       link {
+        id
         votes {
           id
           user {
